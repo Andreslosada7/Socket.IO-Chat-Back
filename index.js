52956693cb5117c5f8aa8df6e69afda0bbc20cc4
Andrés Losada <andreslosada7@gmail.com>
2024-04-05 13:52:53 -0500

    Feat: cleaned inused code.

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import express from "express";
 import http from "http";
 import morgan from "morgan";
 import { Server as SocketServer } from "socket.io";
-import { resolve } from "path";
 import cors from "cors";
 
 // Initializations
@@ -19,18 +18,16 @@ app.use(cors());
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: false }));
 
-app.use(express.static(resolve("frontend/dist")));
-
 io.on("connection", (socket) => {
   console.log(socket.id);
   socket.on("message", (message) => {
     socket.broadcast.emit("message", {
       body: message.body,
       from: message.from,
-      /*  from: socket.id.slice(8), */
     });
   });
 });
+
 const PORT = process.env.PORT || 3000;
 server.listen(PORT);
 console.log(`server on port ${PORT}`);
